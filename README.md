4f6cace368459b0117cd252f990bd375071bb237
Jason De Lorme <jasondel@google.com>
2025-03-04 09:16:23 -0500

    Initial check-in, scaffolding of Shipping service.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Acme Shipping Service
+
+The goal for this Spring Boot Application is to provide a REST API for managing package shipments.
---
+++ src/main/java/com/acme/shippingservice/ShippingApplication.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2025 ACME Corp.
+ *
+ * This work is protected by copyright law and international treaties.
+ * Unauthorized reproduction or distribution of this work, or any portion
+ * thereof, may result in severe civil and criminal penalties, and will be
+ * prosecuted to the maximum extent possible under the law.
+ *
+ * ACME Corp. Confidential. All Rights Reserved.
+ *
+ * This software contains the proprietary and confidential information of
+ * ACME Corp., and may not be copied, reproduced, or distributed in any
+ * form without the express written permission of ACME Corp.
+ */
+
+package com.acme.shippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingApplication.class, args);
+	}
+
+}
